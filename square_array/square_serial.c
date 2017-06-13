f476524b2e102ae53958516c41f8905874de181e
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2017-06-13 18:40:21 -0300

    serial version of square

---
+++ square_array/square_serial.c
@@ -0,0 +1,23 @@
+#define ARRAY_SIZE 100000
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+   int i;
+   float input[ARRAY_SIZE], output[ARRAY_SIZE];
+
+   /* Initialize data */
+   for(i=0; i<ARRAY_SIZE; i++) {
+      input[i] = 1.0f*i;
+   }
+
+   // Perform computation
+   for (i=0; i<ARRAY_SIZE; i++) {
+      output[i]=input[i]*input[i];
+   }
+
+   return 0;
+}
