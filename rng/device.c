9f986b3a91541a7910eae7c9cb2add2f871fcb03
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2017-07-13 14:50:56 -0300

    figuring out how to include headers in a kernel

--- rng/device.c
+++ rng/device.c
@@ -14,6 +14,8 @@ int main(int argc, char *argv[]) {
     cl_mem in_d, out_d; 
     size_t streamBufferSize;
     int numWorkItems;
+    float *out_h;
+    int i;
 
     // read command-line argument
     if ( argc != 2 ) {
@@ -22,8 +24,10 @@ int main(int argc, char *argv[]) {
     } 
     sscanf(argv[1], "%i", &numWorkItems); 
 
-    /* OpenCL
-       ==========   */
+    // dynamically allocate arrays
+    out_h = (float *)malloc(sizeof(float)*numWorkItems); 
+
+    /* OpenCL ==============================================*/
     cl_device_id device;
     cl_context context;
     cl_program program;
@@ -45,7 +49,7 @@ int main(int argc, char *argv[]) {
     states), and an error code. */
     clrngMrg31k3pStream* streams = clrngMrg31k3pCreateStreams(NULL, numWorkItems,
                                &streamBufferSize, (clrngStatus *)&err);
-    check_error(err, "cannot create random stream array");
+    //check_error(err, "cannot create random stream array");
 
     /* Then we create an OpenCL buffer of size streamBufferSize and 
     fill it with a copy of the array of streams, to pass to the device. 
@@ -54,40 +58,44 @@ int main(int argc, char *argv[]) {
     buffer creation is not specific to clRNG, so it is not discussed 
     here). */
     // Create buffer to transfer streams to the device.
-    cl_mem in_d = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, 
+    in_d = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, 
                                     streamBufferSize, streams, &err);
     // Create buffer to transfer output back from the device.
-    cl_mem out_d = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, 
-                                     numWorkItems * sizeof(cl_float), NULL, &err);
+    out_d = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, 
+                                     numWorkItems * sizeof(cl_float), out_h, &err);
 
-xxxxxxxxxxxx
     /* Kernel setup */
     kernel = clCreateKernel(program, KERNEL_FUNC, &err);
-    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &xa_d); 
-    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &ya_d); 
-    err |= clSetKernelArg(kernel, 2, sizeof(unsigned int), &ntarget);
+    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &in_d); 
+    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &out_d); 
+    err |= clSetKernelArg(kernel, 2, sizeof(unsigned int), &numWorkItems);
     
     // Get the maximum work group size for executing the kernel on the device
     err = clGetKernelWorkGroupInfo(kernel, device, CL_KERNEL_WORK_GROUP_SIZE, sizeof(localsize), &localsize, NULL);
     // Number of total work items - localSize must be devisor
-    globalsize = ceil(ntarget/(float)localsize)*localsize;
-    printf("global size=%u, local size=%u\n", globalsize, localsize);
+    globalsize = ceil(numWorkItems/(float)localsize)*localsize;
+    printf("global size=%lu, local size=%lu\n", globalsize, localsize);
 
     /* Enqueue kernel   */
     err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalsize, &localsize, 0, NULL, NULL); 
     clFinish(queue);
 
     /* Read the kernel's output    */
-    clEnqueueReadBuffer(queue, xa_d, CL_TRUE, 0, bytes, xa, 0, NULL, NULL); 
-    clEnqueueReadBuffer(queue, ya_d, CL_TRUE, 0, bytes, ya, 0, NULL, NULL); 
+    clEnqueueReadBuffer(queue, out_d, CL_TRUE, 0, numWorkItems*sizeof(float), out_h, 0, NULL, NULL); 
 
     /* Deallocate resources */
     clReleaseKernel(kernel);
-    clReleaseMemObject(xa_d);
-    clReleaseMemObject(ya_d);
+    clReleaseMemObject(in_d);
+    clReleaseMemObject(out_d);
     clReleaseCommandQueue(queue);
     clReleaseProgram(program);
     clReleaseContext(context);
+    /* End of OpenCL routines ==============================================*/
+
+    printf("Random numbers generated in GPU:\n");
+    for (i=0; i<numWorkItems; i++) {
+        printf("%f ", out_h[i]);
+    }
 
 
 	return(0);	
--- rng/kernel.cl
+++ rng/kernel.cl
@@ -1,12 +1,28 @@
+/* 
+Pointers to the global memory buffers received from the host and to 
+the output array are passed to the kernel as arguments. (The correspondence 
+between the kernel arguments and the buffers on the host is specified in 
+the host code, not shown here). 
+
+For each work item, we create a private 
+copy of its stream, named private_stream, in its private memory, so we can 
+generate random numbers on the device. The private memory must be allocated 
+at compile time; this is why private_stream is not declared as a pointer, so 
+the declaration allocates memory. The kernel just generates two random numbers 
+and returns the sum, in a cl_double.
+*/
+
+
 #include <clRNG/mrg31k3p.clh>
 
-__kernel void example(__global clrngMrg31k3pHostStream* streams, __global float* out) {
+__kernel void example(__global clrngMrg31k3pHostStream* streams, __global float* out, const int n) {
     int gid = get_global_id(0);
 
-    clrngMrg31k3pStream private_stream_d;   // This is not a pointer!
+	if (gid<n) {
+    	clrngMrg31k3pStream private_stream_d;   // This is not a pointer!
 
-    clrngMrg31k3pCopyOverStreamsFromGlobal(1, &private_stream_d, &streams[gid]);
+    	clrngMrg31k3pCopyOverStreamsFromGlobal(1, &private_stream_d, &streams[gid]);
 
-    out[gid] = clrngMrg31k3pRandomU01(&private_stream_d) +
-               clrngMrg31k3pRandomU01(&private_stream_d);
+    	out[gid] = clrngMrg31k3pRandomU01(&private_stream_d);
+    }
 }
